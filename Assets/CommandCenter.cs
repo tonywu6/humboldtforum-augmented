f066eb08284e4daf552ccf86c5cad8c912fb35e8
Tony Wu <tony.wu@nyu.edu>
2019-07-04 07:56:06 +0200

    a lot

--- Assets/CommandCenter.cs
+++ Assets/CommandCenter.cs
@@ -10,100 +10,50 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using SimpleJSON;
 using System.IO;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using SimpleJSON;
 
 public class CommandCenter : MonoBehaviour
 {
 
-    public class MuseumObject
-    {
-        public string id;
-        public Vector4 location;
-        public string type;
-        public string name;
-
-        public List<string> themes;
-        public List<string> exhibitions;
-        public Color32 color = new Color32(1, 1, 1, 1);
-
-        public List<string> exhibits;
-        public int navHierarchy;
-
-        public MuseumObject(JSONNode json)
-        {
-            id = json["indices"]["id"];
-            location = DatabaseLocationToVectorLocation(json["indices"]["locationIdentifier"]);
-            type = json["indices"]["type"];
-            name = json["description"]["name"];
-
-            switch (type)
-            {
-                case "exhibit":
-                    JSONNode curatorialInfo = json["curatorial"];
-                    themes = new List<string>();
-                    exhibitions = new List<string>();
-                    foreach (JSONNode theme in curatorialInfo["themes"].AsArray)
-                    {
-                        themes.Add(theme);
-                    }
-                    foreach (JSONNode exhibition in curatorialInfo["exhibitions"].AsArray)
-                    {
-                        exhibitions.Add(exhibition);
-                    }
-                    JSONArray color32 = curatorialInfo["colors"][0].AsArray;
-                    color = new Color32((byte)color32[0].AsInt, (byte)color32[1].AsInt, (byte)color32[2].AsInt, 255);
-                    break;
-                case "place":
-                    JSONNode relations = json["relations"];
-                    exhibits = new List<string>();
-                    foreach (JSONNode exhibit in relations["exhibits"].AsArray)
-                    {
-                        exhibits.Add(exhibit);
-                    }
-                    navHierarchy = relations["navigationalHierarchy"];
-                    break;
-            }
-        }
-    }
-
     private JSONObject museumCollectionJSON;
 
-    private GameObject museumSpace;
-    private GameObject arMuseumSpace;
+    private GameObject museumWorld;
     public Camera placeholderCam;
 
-    public static Vector4 currentLocation;
-    public static Dictionary<string, MuseumObject> museumObjects = new Dictionary<string, MuseumObject>();
+    public static Dictionary<string, MuseumObjectMetadata> museumExhibits = new Dictionary<string, MuseumObjectMetadata>();
+    public static Dictionary<string, MuseumObjectMetadata> museumSpaces = new Dictionary<string, MuseumObjectMetadata>();
 
-    public static float museumLength = 183.811f;
-    public static float museumWidth = 121.7801f;
+    public static Vector3 museumDimension = new Vector3(183.811f, 40f, 121.7801f);
 
     // Start is called before the first frame update
     void Start()
     {
         LoadMuseumCollection();
-        Debug.Log(museumCollectionJSON["items"]["1994.245.135"]["indices"]["provider"]);
-
-        currentLocation = new Vector4(0.2445209768f, 0, 0.4865384615f, 0);
-
         UnloadAllScenesExcept("Controller");
     }
     private void LoadMuseumCollection()
     {
-        string filePath = Path.Combine(Application.streamingAssetsPath, "objects.json");
+        string filePath = Path.Combine(Application.streamingAssetsPath, "metadata.json");
         string jsonStr = File.ReadAllText(filePath);
         museumCollectionJSON = (JSONObject)JSON.Parse(jsonStr);
 
-        foreach (JSONNode item in museumCollectionJSON["exhibits"])
+        foreach (JSONNode item in museumCollectionJSON["objects"])
         {
-            museumObjects.Add(item["indices"]["id"], new MuseumObject(item));
-        }
-        foreach (JSONNode item in museumCollectionJSON["places"])
-        {
-            museumObjects.Add(item["indices"]["id"], new MuseumObject(item));
+            switch ((string)item["type"])
+            {
+                case "exhibit":
+                    museumExhibits.Add(item["id"], new MuseumObjectMetadata(item));
+                    break;
+                case "space":
+                    museumSpaces.Add(item["id"], new MuseumObjectMetadata(item));
+                    break;
+                default:
+                    print("What");
+                    break;
+            }
         }
     }
 
@@ -119,7 +69,6 @@ public class CommandCenter : MonoBehaviour
         for (int i = 0; i < c; i++)
         {
             Scene scene = SceneManager.GetSceneAt(i);
-            print(scene.name);
             if (scene.name != sceneName && scene.name != "UnityARKitRemote")
             {
                 SceneManager.UnloadSceneAsync(scene);
@@ -127,23 +76,6 @@ public class CommandCenter : MonoBehaviour
         }
     }
 
-    // - Demonstration Controls
-    public void MapPreview()
-    {
-        if (!SceneManager.GetSceneByName("3DMapScene").isLoaded)
-        {
-            UnloadAllScenesExcept("Controller");
-            StartCoroutine("AddSceneAndSetActive", "3DMapScene");
-        }
-    }
-    public void MapLocalizationPreview()
-    {
-        if (!SceneManager.GetSceneByName("3DMapScene").isLoaded)
-        {
-            UnloadAllScenesExcept("Controller");
-            StartCoroutine("AddSceneAndSetActive", "3DMapScene");
-        }
-    }
     public void ARMapPreview()
     {
         UnloadAllScenesExcept("Controller");
@@ -159,29 +91,11 @@ public class CommandCenter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!museumSpace)
-        {
-            museumSpace = GameObject.Find("Museum");
-        }
-        if (!arMuseumSpace)
-        {
-            museumSpace = GameObject.Find("ARMuseum");
-        }
-
     }
 
     // Helpers
-    public static void ChangeColor(GameObject GO, Color col)
+    public static Vector3 DenormalizedMuseumVectors(Vector3 pos, bool moveAnchor = false)
     {
-        MaterialPropertyBlock propBlock = new MaterialPropertyBlock();
-        Renderer renderer = GO.GetComponent<Renderer>();
-        renderer.GetPropertyBlock(propBlock);
-        propBlock.SetColor("_Color", col);
-        renderer.SetPropertyBlock(propBlock);
-    }
-    public static Vector4 DatabaseLocationToVectorLocation(JSONNode jsonLocation)
-    {
-        JSONArray locationIdentifier = jsonLocation.AsArray;
-        return new Vector4(locationIdentifier[4] - 0.5f, locationIdentifier[0] / 100, -(locationIdentifier[5] - 0.5f), locationIdentifier[0]);
+        return moveAnchor ? Vector3.Scale(pos - new Vector3(.5f, .5f, -.5f), museumDimension) : Vector3.Scale(pos, museumDimension);
     }
 }
