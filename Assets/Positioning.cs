dfc02019f4d7f801f5b7415ffce7aa167175b02b
Tony Wu <tony.wu@nyu.edu>
2019-09-07 12:25:49 -0400

    Improved overlap detection; added movement simulation

---
+++ Assets/Positioning.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Positioning : MonoBehaviour
+{
+
+    public bool cameraIn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        cameraIn = other.gameObject.layer == 14;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        cameraIn = false;
+    }
+}
